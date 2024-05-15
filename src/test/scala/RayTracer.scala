e3bc0a0e3533d507dfd8414cbb15335d32a11f5b
ecwood <wooderi@stanford.edu>
2024-05-15 02:18:42 -0700

    some of the code from lab 3

--- src/test/scala/RayTracer.scala
+++ src/test/scala/RayTracer.scala
@@ -4,7 +4,14 @@ import spatial.dsl._
 
 
   def main(args: Array[String]): Unit = {
-    
+    val width = 400
+    val height = 400
+    val image = (0::width, 0::height){(i,j) => 0}
+
+    val img = DRAM[T](width, height)
+    val imgOut = DRAM[T](width, height)
+
+    setMem(img, image)
     
     Accel {
       
