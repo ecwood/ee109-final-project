5fab275d2431100988906b19a0f2cedb1289fc0d
ecwood <wooderi@stanford.edu>
2024-05-28 22:27:37 -0700

    one other file

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 # Write any command you want to run
-TEST_DATA_HOME=$PWD/unit_tests sbt -Dtest.CS217=true "; testOnly LoadCSV"
+# TEST_DATA_HOME=$PWD/unit_tests sbt -Dtest.CS217=true "; testOnly LoadCSV"
+TEST_DATA_HOME=$PWD/unit_tests sbt -Dtest.CS217=true "; testOnly LoadCSVMultiple"
