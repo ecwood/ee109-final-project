f0e8e576aff8cd0dfdfd5c4694d424a7ae25cb7f
ecwood <wooderi@stanford.edu>
2024-06-01 00:41:44 -0700

    Working square root controller

--- src/test/scala/SquareRoot.scala
+++ src/test/scala/SquareRoot.scala
@@ -11,37 +11,49 @@ import spatial.dsl._
     y: RegType,
     z: RegType
   )
-  def main(args: Array[String]): Unit = {
+  val iterations = 31
 
+  def main(args: Array[String]): Unit = {
     val out = ArgOut[RegType]
 
     Accel {
       val temp = 26.0.to[RegType]
-      val saved_vals = SRAM[RegType](31)
+      val saved_vals = SRAM[RegType](iterations)
 
-      Foreach (1 until 32) { square =>
-        val squared = (square * square).to[SubType]
-        val divided = temp.to[SubType] / squared
-        val shfted = divided.to[SubType] - 1.0
-        val square_root = square.to[SubType] * (1.0 + divided / 2.0 - (divided * divided) / 8.0 + (divided * divided * divided) / 16.0)
-        val prev = square.to[SubType] - 1.0.to[SubType]
-        val save = (divided >= 1 && (temp.to[SubType] / (prev * prev).to[SubType] < 1)).to[RegType]
+      val actual_square_root = Reg[RegType](0.0)
+
+      Foreach (1 until (iterations + 1)) { square =>
+        val squared = (square * square).to[RegType]
+        val square_cast = square.to[RegType]
+        val divided = (temp.to[SubType] / squared.to[SubType]).to[RegType]
+        val shfted = (divided.to[SubType] - 1.0).to[RegType]
+        val shfted_sqed = (shfted.to[SubType] * shfted.to[SubType]).to[RegType]
+        val shfted_tred = (shfted.to[SubType] * shfted.to[SubType] * shfted.to[SubType]).to[RegType]
+        val sqrt_elem1 = 1.0.to[RegType]
+        val sqrt_elem2 = (shfted.to[SubType] / 2.0).to[RegType]
+        val sqrt_elem3 = (shfted_sqed.to[SubType] / 4.0).to[RegType]
+        val sqrt_elem4 = (shfted_tred.to[SubType] / 16.0).to[RegType]
+        val sqrt_series = (sqrt_elem1.to[SubType] + sqrt_elem2.to[SubType] - sqrt_elem3.to[SubType] + sqrt_elem4.to[SubType]).to[RegType]
+        val square_root = (square_cast.to[SubType] * sqrt_series.to[SubType]).to[SubType]
+        val next = (square_cast.to[SubType] + 1.to[SubType]).to[RegType]
+        val next_squared = (next.to[SubType] * next.to[SubType]).to[RegType]
+        val next_divided = (temp.to[SubType] / next_squared.to[SubType]).to[RegType]
+        val save = (divided >= 1 && (next_divided < 1)).to[RegType]
         val value_to_save = (square_root.to[RegType] * save).to[RegType]
-        saved_vals(square - 1) = value_to_save
+        saved_vals(square - 1) = value_to_save.to[RegType]
       }
 
-      val sqrt_ans = 0.0.to[RegType]
-      Reduce (sqrt_ans) (0 until 31) { sq =>
-        saved_vals(sq)
-      }{_+_}
+      Reduce (actual_square_root) (iterations by 1) { sqrt_i =>
+        saved_vals(sqrt_i)
+      } {_+_}
 
-      out := sqrt_ans
+      out := actual_square_root
     }
 
     val result = getArg(out)
 
     // temporary for basic asm tests
-    printArray(result)
+    print(result)
     assert(1 == 1)
   }
 }
