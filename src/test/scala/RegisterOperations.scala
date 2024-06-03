91b06c97b09e7cd8cf75eec1e734d4e3eef2284b
ecwood <wooderi@stanford.edu>
2024-06-03 03:52:10 -0700

    Compare flag added in

--- src/test/scala/RegisterOperations.scala
+++ src/test/scala/RegisterOperations.scala
@@ -56,31 +56,37 @@ import spatial.dsl._
 
       val internal_out = SRAM[RegType](pixel_rows, pixel_columns, num_instructions, num_vec_elements + 1)
 
-      val vec_operations = SRAM[Vector3](num_operations)
-      val sca_operations = SRAM[RegType](num_operations)
-
       val squares_for_square_roots = SRAM[RegType](square_root_table_elements)
 
       Foreach (0 until square_root_table_elements) { num =>
         squares_for_square_roots(num) = square_map_sram(num, 1).to[RegType]
       }
 
+      val vec_operations = SRAM[Vector3](pixel_rows, pixel_columns, num_operations)
+      val sca_operations = SRAM[RegType](pixel_rows, pixel_columns, num_operations)
+
       Foreach (0 until pixel_rows) { row =>
         Foreach (0 until pixel_columns) { col =>
           Foreach (0 until num_instructions) { i =>
+            val vec_compare_choice = SRAM[Vector3](2)
+            val sca_compare_choice = SRAM[RegType](2)
+
             // Decoding the instruction and storing it as an instruction (for clarity)
+            val comp = inst_sram(i, 1) * 8 + inst_sram(i, 2) * 4 + inst_sram(i, 3) * 2 + inst_sram(i, 4)
             val op = inst_sram(i, 5) * 16 + inst_sram(i, 6) * 8 + inst_sram(i, 7) * 4 + inst_sram(i, 8) * 2 + inst_sram(i, 9)
             val src1 = inst_sram(i, 12) * 8 + inst_sram(i, 13) * 4 + inst_sram(i, 14) * 2 + inst_sram(i, 15)
             val src2 = inst_sram(i, 16) * 8 + inst_sram(i, 17) * 4 + inst_sram(i, 18) * 2 + inst_sram(i, 19)
             val dest = inst_sram(i, 20) * 8 + inst_sram(i, 21) * 4 + inst_sram(i, 22) * 2 + inst_sram(i, 23)
             val immediate = inst_sram(i, 10) * 32 + inst_sram(i, 11) * 16 + inst_sram(i, 12) * 8 + inst_sram(i, 13) * 4 + inst_sram(i, 14) * 2 + inst_sram(i, 15)
 
-
             val vec_reg_src1 = vec_regs(row, col, src1.to[Int])
             val vec_reg_src2 = vec_regs(row, col, src2.to[Int])
             val sca_reg_src1 = sca_regs(row, col, src1.to[Int])
             val sca_reg_src2 = sca_regs(row, col, src2.to[Int])
+            val sca_reg_comp = sca_regs(row, col, comp.to[Int])
             val immediate_regtype = immediate.to[RegType]
+
+            val compare_flag = (comp.to[Int] == 0 || sca_reg_comp.to[Int] == 1).to[Int]
 
             val add_vectors = Vector3((vec_reg_src1.x.to[SubType] + vec_reg_src2.x.to[SubType]).to[RegType], (vec_reg_src1.y.to[SubType] + vec_reg_src2.y.to[SubType]).to[RegType], (vec_reg_src1.z.to[SubType] + vec_reg_src2.z.to[SubType]).to[RegType])
 
@@ -140,44 +146,50 @@ import spatial.dsl._
             val addi_vector_y = Vector3(vec_reg_src2.x, immediate_regtype + vec_reg_src2.y, vec_reg_src2.z)
             val addi_vector_z = Vector3(vec_reg_src2.x, vec_reg_src2.y, immediate_regtype + vec_reg_src2.z)
 
-            vec_operations(0) = add_vectors
-            vec_operations(1) = sub_vectors
-            vec_operations(2) = normalize_vector
-            vec_operations(3) = vec_regs(row, col, dest.to[Int])
-            vec_operations(4) = vec_regs(row, col, dest.to[Int])
-            vec_operations(5) = vec_regs(row, col, dest.to[Int])
-            vec_operations(6) = mult_vscalar
-            vec_operations(7) = div_vscalar
-            vec_operations(8) = vec_regs(row, col, dest.to[Int])
-            vec_operations(9) = vec_regs(row, col, dest.to[Int])
-            vec_operations(10) = vec_regs(row, col, dest.to[Int])
-            vec_operations(11) = vec_regs(row, col, dest.to[Int])
-            vec_operations(12) = vec_regs(row, col, dest.to[Int])
-            vec_operations(13) = vec_regs(row, col, dest.to[Int])
-            vec_operations(14) = addi_vector_x
-            vec_operations(15) = addi_vector_y
-            vec_operations(16) = addi_vector_z
+            vec_operations(row, col, 0) = add_vectors
+            vec_operations(row, col, 1) = sub_vectors
+            vec_operations(row, col, 2) = normalize_vector
+            vec_operations(row, col, 3) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 4) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 5) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 6) = mult_vscalar
+            vec_operations(row, col, 7) = div_vscalar
+            vec_operations(row, col, 8) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 9) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 10) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 11) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 12) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 13) = vec_regs(row, col, dest.to[Int])
+            vec_operations(row, col, 14) = addi_vector_x
+            vec_operations(row, col, 15) = addi_vector_y
+            vec_operations(row, col, 16) = addi_vector_z
 
-            sca_operations(0) = sca_regs(row, col, dest.to[Int])
-            sca_operations(1) = sca_regs(row, col, dest.to[Int])
-            sca_operations(2) = sca_regs(row, col, dest.to[Int])
-            sca_operations(3) = src2_mag
-            sca_operations(4) = src2_mag2
-            sca_operations(5) = dot_product
-            sca_operations(6) = sca_regs(row, col, dest.to[Int])
-            sca_operations(7) = sca_regs(row, col, dest.to[Int])
-            sca_operations(8) = sqrt_scalar
-            sca_operations(9) = add_scalar
-            sca_operations(10) = sub_scalar
-            sca_operations(11) = mult_scalar
-            sca_operations(12) = div_scalar
-            sca_operations(13) = addi_scalar
-            sca_operations(14) = sca_regs(row, col, dest.to[Int])
-            sca_operations(15) = sca_regs(row, col, dest.to[Int])
-            sca_operations(16) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 0) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 1) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 2) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 3) = src2_mag
+            sca_operations(row, col, 4) = src2_mag2
+            sca_operations(row, col, 5) = dot_product
+            sca_operations(row, col, 6) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 7) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 8) = sqrt_scalar
+            sca_operations(row, col, 9) = add_scalar
+            sca_operations(row, col, 10) = sub_scalar
+            sca_operations(row, col, 11) = mult_scalar
+            sca_operations(row, col, 12) = div_scalar
+            sca_operations(row, col, 13) = addi_scalar
+            sca_operations(row, col, 14) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 15) = sca_regs(row, col, dest.to[Int])
+            sca_operations(row, col, 16) = sca_regs(row, col, dest.to[Int])
 
-            vec_regs(row, col, dest.to[Int]) = vec_operations(op.to[Int])
-            sca_regs(row, col, dest.to[Int]) = sca_operations(op.to[Int])
+            vec_compare_choice(0) = vec_regs(row, col, dest.to[Int])
+            vec_compare_choice(1) = vec_operations(row, col, op.to[Int])
+
+            sca_compare_choice(0) = sca_regs(row, col, dest.to[Int])
+            sca_compare_choice(1) = sca_operations(row, col, op.to[Int])
+
+            vec_regs(row, col, dest.to[Int]) = vec_compare_choice(compare_flag)
+            sca_regs(row, col, dest.to[Int]) = sca_compare_choice(compare_flag)
 
             internal_out(row, col, i, 0) = vec_regs(row, col, 1).x
             internal_out(row, col, i, 1) = vec_regs(row, col, 1).y
